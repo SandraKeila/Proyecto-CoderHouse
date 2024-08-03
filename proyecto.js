38b1dbb748ca7457e66acd4ea9b827befb9d64c6
SandraKeila <sandraterres0@gmail.com>
2024-08-03 04:44:31 -0500

    Update proyecto.js

--- proyecto.js
+++ proyecto.js
@@ -1,19 +1,52 @@
-let ingresos = Number(prompt("Ingresar la cantidad de tus ingresos mensuales"));
-let gasto = prompt("Mencione en qué realizó un gasto");
-let cantidadGasto = Number(prompt("Ingresar la cantidad del gasto"));
-let masgastos = prompt("¿realizaste otro gasto más?(solo si/no)");
 
-while (masgastos !== "no") {
-  let otrogasto = prompt("Mencione en qué realizó un gasto");
-  let otrocantidadGasto = Number(prompt("Ingresar la cantidad del gasto"));
-  cantidadGasto = cantidadGasto + otrocantidadGasto;
-  masgastos = prompt("¿realizaste otro gasto más?(solo si/no)");
-}
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-if (ingresos - cantidadGasto > 0) {
-  alert(
-    "El dinero que le queda es de " + (ingresos - cantidadGasto) + ", puede crear una cuenta de ahorros para guardar su dinero");
-} else {
-  alert(
-    "El dinero que le queda es de " + (ingresos - cantidadGasto) + ", le recomendamos llevar cursos de salud financiera");
-}
+let contador = 1;
+let addTodoButton = document.getElementById("btn-add"); 
+let todosList = document.getElementById("todo-list"); 
+let totalSumaElement = document.getElementById("total-suma");
+
+let addTodo = () => {
+  let todoText = prompt("Nombre del gasto");
+  let todoprecio = Number(prompt("Cantidad de gasto"));
+  let todo = { id: contador, text: todoText, number: todoprecio };
+  todos.push(todo);
+
+  contador++;
+
+  pintarTarjetas();
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+addTodoButton.addEventListener("click", addTodo);
+
+const deleteTodo = (elemento) => {
+  todos = todos.filter((todo) => todo.id !== elemento.id); 
+  pintarTarjetas();
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const pintarTarjetas = () => {
+  todosList.innerHTML = "";
+
+  todos.forEach((elemento) => {
+    let item = document.createElement("li");
+    item.className = "card";
+    item.innerHTML = `<span>${elemento.text}</span> 
+      <span>${elemento.number}</span> 
+      <button class="delete-button">Borrar</button>`;
+
+    let deleteButton = item.querySelector(".delete-button");
+
+    deleteButton.addEventListener("click", () => {
+      deleteTodo(elemento);
+    });
+
+    todosList.appendChild(item);
+  });
+
+  let totalSuma = todos.reduce((sum, todo) => sum + todo.number, 0);
+  totalSumaElement.textContent = totalSuma.toFixed(2);
+};
+
+pintarTarjetas();
